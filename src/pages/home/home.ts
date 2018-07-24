946f3beafde568288a050cd0025adf45b0865882
Teerasej <teerasej@gmail.com>
2018-07-25 00:25:58 +0700

    finish

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { BarcodeScanner } from '@ionic-native/barcode-scanner';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  barcodeText = "";
+  barcodeFormat = "";
 
+  constructor(public navCtrl: NavController, public scanner : BarcodeScanner) {
+
+  }
+
+  startScan() {
+    this.scanner.scan().then(
+      (resultData) => {
+        this.barcodeText = resultData.text;
+        this.barcodeFormat = resultData.format;
+      }
+    )
   }
 
 }
